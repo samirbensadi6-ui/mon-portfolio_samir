98b2334484a4c02211ab5d4ac9aa5d355db5a9ba
samirbensadi6-ui <samirbensadi6@gmail.com>
2026-08-29 17:28:47 +0200

    portfolio

---
+++ js/script.js
@@ -0,0 +1,59 @@
+const sliders = document.querySelectorAll(".projet-slides");
+
+sliders.forEach((slider) => {
+
+  const slides = slider.querySelectorAll(".projet-image");
+  const dots = slider.querySelectorAll(".dot");
+  const prevBtn = slider.querySelector(".slider-prev");
+  const nextBtn = slider.querySelector(".slider-next");
+
+  let currentSlide = 0;
+
+  function showSlide(index) {
+
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+
+    dots.forEach((dot) => {
+      dot.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+
+    currentSlide = index;
+  }
+
+  nextBtn.addEventListener("click", () => {
+
+    currentSlide++;
+
+    if (currentSlide >= slides.length) {
+      currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+  });
+
+  prevBtn.addEventListener("click", () => {
+
+    currentSlide--;
+
+    if (currentSlide < 0) {
+      currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+  });
+
+  dots.forEach((dot, index) => {
+
+    dot.addEventListener("click", () => {
+      showSlide(index);
+    });
+
+  });
+
+  showSlide(0);
+});
